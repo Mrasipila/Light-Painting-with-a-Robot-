b774fbdec93fa2ea766861e5055a4e5b0e2991d5
RaphyStark <62150391+RaphyStark@users.noreply.github.com>
2021-03-26 16:01:14 +0100

    forward, arrest, nextpoint etc

--- CC/CC.ino
+++ CC/CC.ino
@@ -1,133 +1,182 @@
 /*-----------------------------------------
-  CC.ino (v3)
-  Utilisation de la librairie Encoder 
-  qui remplace l’implémentation des interruptions, 
-  librairie PID _v2
+  CC.ino (v4)
+    PID_v2,
+    Tableau de coordonnées,
+    Implémentation fonction nextPoint
+    Pour passer d’un couple de coordonnées à un autre
 -----------------------------------------*/
 
-#include <PID_v2.h>
+#include <FastPID.h>
 #include <motor.h>
 #include <Encoder.h>
-#define R 3.5
-#define L 15.0
-#define N 1440.0
-#define DEB0 1
-#define DEB1 0
 
+/* Pour les neopixels */
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+#define PIN       8
+#define NUMPIXELS 7
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+// left and right motors with corresponding pins
+Motor leftMotor(leftMotorPwmPin, leftMotorDirPin);
+Motor rightMotor(rightMotorPwmPin, rightMotorDirPin);
+
+// left and right encoders with corresponding pins
+Encoder leftEnc(19, 18);
+Encoder rightEnc(20, 21);
+
+// Configuration PID
+float Kp = 0.9, Ki = 0, Kd = 0, Hz = 1;
+int output_bits = 16;
+bool output_signed = false;
+FastPID myPID(Kp, Ki, Kd, Hz, output_bits, output_signed);
+
+// Rayon de la roue
+#define R 3.5
+// Distance entre les deux roues
+#define L 15.0
+// Nombre de ticks pour un tour
+#define N 1440.0
+
+/* Variables nécessaires pour calcul du déplacement */
+// Diff entre nombre de ticks attendus et réels
 int deltatickG = 0;
 int deltaprimeG = 0;
 int deltatickD = 0;
 int deltaprimeD = 0;
+
+// Distance parcourue par roue
 double DCg = 0;
 double DCd = 0;
+
+// Distance parcourue
 double DC = 0;
+
+// Angle entre position actuelle
+// et position souhaité
 double phi = 0;
+
+// Coordonnées initiales
 double x = 0;
 double y = 0;
-double desiredA;
-int mark = 0;
 
-// PID
+// Coordonnées désirées
+double xd = 10;
+double yd = 10;
 
-//Define Variables we'll be connecting to
-// slw : setpoint left wheel
-// ilw : input left wheel
-// olw : output left wheel
-double slw, ilw, olw;
-double srw, irw, orw;
+// Angle désiré
+double desiredA, desA;
+//
+int input = 0;
+// int mark = 0;
 
-//Specify the links and initial tuning parameters
-PID rwPID(&irw, &orw, &srw, 40, 0, 0, DIRECT);
-PID lwPID(&ilw, &olw, &slw, 40, 0, 0, DIRECT);
-
-
-// MOTOR : left and right motors with corresponding pins
-Motor leftMotor(leftMotorPwmPin, leftMotorDirPin);
-Motor rightMotor(rightMotorPwmPin, rightMotorDirPin);
+// Fonction de contrôle du mouvement du robot
+void rotate(double phi, double desiredA);
+void forward(double x, double y, double desiredx, double desiredy);
+// void curve(...)
+// left_motor -> rwspeed
 
 
-// ENCODERS
-Encoder leftEnc(19, 18);
-Encoder rightEnc(20, 21);
-
-// TIMER
-double temprime;
-double t;
-double temps;
-double setpoint;
-double delta;
-
-
-void setup() 
+void setup()
 {
-  //   put your setup code here, to run once:
   Serial.begin(9600);
-  double xd = 10;
-  double yd = 10;
-  setpoint = atan2(yd - y, xd - x);
-  
-  rwPID.SetMode(AUTOMATIC);
-  lwPID.SetMode(AUTOMATIC);
-  temprime = millis();
-  t = 0;
 
-  // 
+  // Calcul l'angle entre xd et x :
+  desiredA = atan2(yd - y, xd - x);
+  Serial.println("setup desiredA :");
+  Serial.println(desiredA);
+  Serial.println(desiredA);
+  Serial.println(desiredA);
+  
+  
+
+  // affInfos();
+  // delay(1000);
+
+  if (myPID.err())
+  {
+    Serial.println("There is a configuration error!");
+    for (;;) {}
+  }
+  myPID.setOutputRange(-255, 255);
 }
 
-void loop() 
+
+void loop()
 {
-  // Nombre de ticks entre deux itérations
+  // Nbre de ticks fait entre deux itérations
   deltatickG = leftEnc.read() - deltaprimeG;
   deltaprimeG = leftEnc.read();
   deltatickD = rightEnc.read() - deltaprimeD;
-  deltaprimeD = rightEnc.read(); 
+  deltaprimeD = rightEnc.read();
 
-  Serial.print("deltatickG: ");
-  Serial.println(deltatickG);
-  Serial.print("deltatickD: ");
-  Serial.println(deltatickD);
+  // print infos
 
-  // Temps écoulé entre deux itérations
-  // temprime : temps du setup
-  temps = millis() - temprime;
-  delta = temps - t;
-  t = temps;
-
+  //Distance parcourue des roues :
   DCg = 2 * PI * R * (deltatickG / N);
   DCd = 2 * PI * R * (deltatickD / N);
 
+  // Distance parcourue du robot
   DC = (DCg + DCd) / 2;
+
+  // Mise à jour des coordonnées
   x = x + DC * cos(phi);
   y = y + DC * sin(phi);
+  // Calcul de l'angle
   phi = phi + (DCd - DCg) / L;
+  desA = desiredA * 100;
+  input = phi * 100;
 
-  rightWspeed(DCd / delta, 30);
-  leftWspeed(DCd / delta, 30);
+  rotate(input, desA);
 
-  if (DEB0) 
+  // forward(x, y, 10, 10);
+}
+
+void rotate(int input, double desA)
+{
+  uint8_t output = myPID.step(desA, input);
+  rightMotor.setU(-output);
+  leftMotor.setU(output);
+}
+
+void forward(double x, double y, double desiredx, double desiredy)
+{
+  leftMotor.setU(40);
+  rightMotor.setU(40);
+  arret(x, y, desiredx, desiredy);
+}
+
+
+void arret(double x, double y, double desiredx, double desiredy)
+{
+  if (x > desiredx - 2 && x < desiredx + 2 && y > desiredy - 2 && y < desiredy + 2)
   {
-    Serial.print("x : ");
-    Serial.println(x);
-    Serial.print("y : ");
-    Serial.println(y);
-    Serial.print("phi : ");
-    Serial.println(phi);
+    leftMotor.setU(0);
+    rightMotor.setU(0);
+    delay(1000);
   }
 }
 
-void rightWspeed(double actualSpeed, double desiredSpeed)
+void affInfos()
 {
-  irw = actualSpeed;
-  srw = desiredSpeed;
-  rwPID.Compute();
-  Serial.println(orw);
-  rightMotor.setU(orw);
-}
+  Serial.println(input);
+  Serial.println(desA);
 
-void leftWspeed(double actualSpeed, double desiredSpeed) {
-  ilw = actualSpeed;
-  slw = desiredSpeed;
-  lwPID.Compute();
-  Serial.println(olw);
-  leftMotor.setU(olw);
+  Serial.print("input :");
+  Serial.println(input);
+
+  Serial.print("x : ");
+  Serial.println(x);
+  Serial.print("y : ");
+  Serial.println(y);
+  Serial.print("phi : ");
+  Serial.println(phi);
+
+  Serial.print("desiredA :");
+  Serial.println(desiredA);
+  Serial.println(x);
+  Serial.println(y);
 }
